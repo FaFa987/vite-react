f6f7ca38721a1774f6a658d42732a7bac8226224
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-13 00:41:00 +0100

    export const App = () => {} { App }

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => { 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        test test test test
-      </p>
-    </>
-  )
-}
+    <header class = "bg-primary text-white text-center py-5">
+    <div>
+      <h1>Hello this is simple Header</h1>
+      <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development to fill empty spaces in a layout that does not yet have content.</p>
+    </div>
+    </header>
+  );
 
-export default App
+};
+

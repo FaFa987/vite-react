0414399a4cc106b639d412e4c9a50035d5bfdd48
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-13 02:10:42 +0100

    Content.jsx/Header.jsx/Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@
 export const App = () => { 
 
   return (
-    <header class = "bg-primary text-white text-center py-5">
-    <div>
-      <h1>Hello this is simple Header</h1>
-      <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development to fill empty spaces in a layout that does not yet have content.</p>
-    </div>
-    </header>
+    <>
+
+    </>
   );
 
 };

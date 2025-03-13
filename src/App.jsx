3f532742848ec1f1ea400bbf6f5bc52390a69dd0
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-13 02:20:29 +0100

    <Navbar /> <Header /> <Content />

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from "./Header";
+import Content from "./Content";
+import Navbar from "./Navbar";
 
 export const App = () => { 
 
   return (
     <>
-
+    <Navbar />
+    <Header />
+    <Content />
     </>
   );
 

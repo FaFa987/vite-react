0917aef2c2f70910e25c16e7fd08f93da140039e
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-21 14:25:24 +0100

    DynamicNavbar --> const  = [{value}]

---
+++ src/DynamicNavbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const naveLinks = [
+  { id: 1 , name: "Home" , href: "#"},
+  { id: 2, name: "Features", href: "#" },
+  { id: 3, name: "Pricing", href: "#" },
+  { id: 4, name: "Contact", href: "#" },
+];
+
+const DynamicNavbar = () => {
+    return (
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            MyApp
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              
+              ??? DynamicNavbar
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+};
+
+export default DynamicNavbar;

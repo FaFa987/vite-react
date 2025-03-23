80bb2916a36fa36422d9bceb1be961531eb0f519
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-23 20:00:27 +0100

    props --> ({ title, ...})

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-export const Card = (props) => {
+export const Card = ({img, title, description, buttonAction}) => {
     return (
         <div>
 
             <div className='card container my-5' style={ {width : "18rem"}}>
-                <img src= {props.img} />
-                <div className='card-title'>{props.title}</div>
-                <div className='card-text'>{props.description}</div>
+                <img src= {img} />
+                <div className='card-title'>{title}</div>
+                <div className='card-text'>{description}</div>
                 <ul>
                     <li>some</li>
                 </ul>
 
-                <button className='btn btn-primary' onClick={props.buttonAction} >Click Here</button>
+                <button className='btn btn-primary' onClick={buttonAction} >Click Here</button>
             </div>
             
         </div>

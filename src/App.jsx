d763d79d9f67d5e78e75fa148d17a975def4ab4b
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-23 15:20:54 +0100

    className={`alert ${props.alertType}`} {props.message}

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import AlertMessage from "./AlertMessage";
 export const App = () => {
   return (
     <>
-    <AlertMessage alertType= "alert-success"/>
-    <AlertMessage alertType= "alert-danger"/>
+    <AlertMessage alertType= "alert-success" message ="Operation was Successfull!"/>
+    <AlertMessage alertType= "alert-danger" message ="Unsuccessfull Operation!"/>
+    <AlertMessage alertType= "alert-info" message ="Information here"/>
 
     </>
   );

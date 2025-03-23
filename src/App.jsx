39efbb2ceebfaf9914ad338aca267408fbc426a5
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-23 15:36:49 +0100

    with variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import AlertMessage from "./AlertMessage";
 
 
 export const App = () => {
+
+  const apiResponse = {
+    alertType : "alert-success",
+    message : "Operation was done!"
+  }
   return (
     <>
-    <AlertMessage alertType= "alert-success" message ="Operation was Successfull!"/>
+    <AlertMessage alertType= {apiResponse.alertType} message ={apiResponse.message}/>
+    
     <AlertMessage alertType= "alert-danger" message ="Unsuccessfull Operation!"/>
     <AlertMessage alertType= "alert-info" message ="Information here"/>
 

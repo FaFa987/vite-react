548fb0ad4fd463f59e192a88afd68b11028e4c44
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-23 19:51:53 +0100

    function --> const

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ export const App = () => {
     status : 400,
     message : "Operation was unsuccess!",
   }
+
+  const handleClick = (msg) => {
+     alert(msg);
+  }
   
   return (
     <>  
 
     <Card title="Demo1" description="Demo description 1" img="https://placehold.co/150" buttonAction= { () => alert("Hello there")} />
-    <Card title="Demo2" description="Demo description 2" img="https://placehold.co/200" />
+    <Card title="Demo2" description="Demo description 2" img="https://placehold.co/200" buttonAction= { () => handleClick("Goodbye")}/>
 
     {/* <AlertMessage alertType="alert-success" message ={apiResponse.message}/>
 

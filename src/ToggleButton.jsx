6de73df370cbb869c08076bdbb6d6381f434e3fe
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-28 21:49:33 +0100

    const [isDarkMode , setIsDarkMode] = useState(true) --> nav

--- src/ToggleButton.jsx
+++ src/ToggleButton.jsx
@@ -1,30 +1,33 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToggleButton = () => {
+
+
+   const [isDarkMode , setIsDarkMode] = useState(true)
+
     return (
         <div className="container-fluid mt-3">
             
-            <nav className='navbar navbar-dark bg-dark'>
+            <nav className = {`navbar ${
+              isDarkMode ? "navbar-dark bg-dark" : " navbar-light bg-light"
+            }`}>
               <div className="container-fluid">
-              <img src='https://placehold.co/64x64/000000/FFFFFF?text=Dark&font=Poppins' alt='logo' />
-              <h3 className='text-light'>Dark Mode</h3>
+              <img src={
+                isDarkMode ? "https://placehold.co/64x64/000000/FFFFFF?text=Dark&font=Poppins"
+                 : "http://placehold.co/64x64/FFFFFF/000000?text=Light&font=Poppins"
+              } alt='logo' />
+              <h3 className={isDarkMode ? "text-light" : "text-dark"}>{isDarkMode ? "Dark Mode" : "Light Mode"}</h3>
               </div>
             </nav>
 
-            <nav className='navbar navbar-light bg-light my-3'>
-              <div className="container-fluid">
-              <img src='http://placehold.co/64x64/FFFFFF/000000?text=Light&font=Poppins' alt='logo' />
-              <h3 className='text-dark'>Light Mode</h3>
-              </div>
-            </nav>
 
             <div className="container text-center py-3">
                 <h1>Drark Mode is on</h1>
                 <h1>Light Mode is on</h1>
 
-                <button className='btn btn-primary mx-1'>Toggle LightMode</button>
-                <button className='btn btn-dark mx-1'>Toggle DarkMode</button>
+                <button className='btn btn-primary mx-1' onClick={() => setIsDarkMode(false)}>Toggle LightMode</button>
+                <button className='btn btn-dark mx-1' onClick={() => setIsDarkMode(true)}>Toggle DarkMode</button>
             </div>
             
         </div>

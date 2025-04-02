0521bb62f597308aae6c029946b4e21cf7f6780e
Fatemeh <f.khalleghi.777@gmail.com>
2025-04-02 16:36:10 +0200

    part1-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./Navbar";
 
-import ToggleButton from "./ToggleButton";
+
 
 export const App = () => {
   return (
     <>
-      <ToggleButton />
+      <Navbar />
     </>
   );
 };
